588d7d9afcae6aa1e0317cc2838d6697310c6db6
Andreev Alexander <andreevlex.as@gmail.com>
2017-05-19 11:56:01 +0300

    Работа с классом urldatabase

--- src/urldatabase.h
+++ src/urldatabase.h
@@ -12,43 +12,19 @@ class QTableView;
 class QSqlTableModel;
 class QSqlQueryModel;
 
-namespace Udb {
-    typedef std::vector<RefRecord> ListRefs;
-}
-
 class UrlDatabase: public QObject
 {
     Q_OBJECT
 public:
 
-    UrlDatabase(QTableView * refRecords, QObject *parent = 0);
+    UrlDatabase(QTableView * refRecords, QTableView * tags, QObject *parent = 0);
     virtual ~UrlDatabase();
-
-    bool saveChangesDB();
-    void Update();
-
-    bool addRef(RefRecord& newref_);
-    bool delRef(const RefRecord& delref_);
-    size_t refsCount(void) const;
-    const RefRecord& getRef(const long long int id_, bool& refFind) const;
-
-    size_t favoriteRefsCount(void) const;
-
-    bool addTag(const RefRecord& parent_, const Tag& newtag_);
-    bool delTag(const RefRecord& parent_, const Tag deltag_);
-    size_t tagsCount(void) const;
-
-    Udb::ListRefs* getRefs(void);
-    Udb::ListRefs* getFavoriteRefs(void);
-    //Udb::ListTags* getUniqTags(void);
 
 protected:
 
-    QSqlRecord m_rec;
-    QSqlTableModel *m_RefRecordModel;
+    QSqlTableModel *RefRecordsModel_m;
+    QSqlTableModel *TagsModel_m;
 private:
-
-    Udb::ListRefs Refs;
 };
 
 #endif // URLDATABASE_H
